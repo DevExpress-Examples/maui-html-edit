fa9dde4c357c0f264ae04665f5cb8dc394fff1de
Anthony Sam <anton.sameishchev@devexpress.com>
2023-12-19 16:09:40 +0400

    Update MauiProgram.cs

--- CS/ReplicateBuiltInToolbar/MauiProgram.cs
+++ CS/ReplicateBuiltInToolbar/MauiProgram.cs
@@ -13,7 +13,6 @@ public static class MauiProgram {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
-
 #if DEBUG
         builder.Logging.AddDebug();
 #endif
