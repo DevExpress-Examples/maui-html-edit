d60153a72227a735a924cc91a3ca6cacdb245504
Anthony Sam <anton.sameishchev@devexpress.com>
2024-04-02 12:19:16 +0400

    Apply suggestions from code review

--- CS/PropertyDescription/ViewModels/PropertyDetailFormViewModel.cs
+++ CS/PropertyDescription/ViewModels/PropertyDetailFormViewModel.cs
@@ -7,17 +7,12 @@ namespace PropertyDescriptionHTMLEdit.ViewModels;
 
 public class PropertyDetailFormViewModel : DetailFormViewModel
 {
-    // private PropertyViewModel customersViewModel => (PropertyViewModel)DataControlContext;
-    // private CrmContext crmContext => customersViewModel.CrmContext;
 
-    // public Command<bool?> EditCommand { get; }
     public Property Property
     {
         get => ((Property)Item);
         set
         {
-            // ((Property)Item).Employee = value;
-            // crmContext.SaveChanges();
             OnPropertyChanged(nameof(Property));
         }
     }
